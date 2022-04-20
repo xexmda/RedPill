a35165838482ead7ef68f54cbd163d3a8459b635
Andrei <andrew4635@yandex.ru>
2022-04-20 19:49:24 +0300

    1 fragment

--- app/src/main/java/com/example/redpill/MainActivity.kt
+++ app/src/main/java/com/example/redpill/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.redpill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.example.redpill.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding?.bottomNav?.setOnNavigationItemSelectedListener(this)
+        binding?.bottomNav?.selectedItemId = R.id.homeItemBN
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.homeItemBN -> supportFragmentManager.beginTransaction().replace(R.id.content, HomeFragment()).commit()
+            R.id.accountItemBN -> supportFragmentManager.beginTransaction().replace(R.id.content, AccountFragment()).commit()
+            R.id.settingsItemBN -> supportFragmentManager.beginTransaction().replace(R.id.content, SettingsFragment()).commit()
+        }
+        return true
     }
 }
